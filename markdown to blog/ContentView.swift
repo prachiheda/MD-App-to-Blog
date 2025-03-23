86c0378f78b0292292e6de4d08600c76efb62360
Prachi Heda <prachiheda@gmail.com>
2025-03-22 22:48:34 -0700

    Initial commit

---
+++ markdown to blog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  markdown to blog
+//
+//  Created by Prachi Heda on 3/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ markdown to blog/markdown_to_blogApp.swift
@@ -0,0 +1,17 @@
+//
+//  markdown_to_blogApp.swift
+//  markdown to blog
+//
+//  Created by Prachi Heda on 3/22/25.
+//
+
+import SwiftUI
+
+@main
+struct markdown_to_blogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
